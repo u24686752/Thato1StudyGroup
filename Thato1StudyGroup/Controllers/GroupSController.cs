47607c0fa79ef964e6a0a14c8498794c7051180a
Thato Mokgothu <u24686752@up.ac.za>
2025-03-03 11:36:37 +0200

    Everything Works

---
+++ Thato1StudyGroup/Controllers/GroupSController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Thato1StudyGroup.Controllers
+{
+    public class GroupSController : Controller
+    {
+        // GET: Group
+        public ActionResult ViewStudy1()
+        {
+            List<Models.MyStudy> people = new List<Models.MyStudy>();
+
+            people.Add(new Models.MyStudy { FirstName = "Thato", LastName = "Mokgothu", Email = "u24686752@tuks.co.za", StudentNumber = "24686752" });
+            people.Add(new Models.MyStudy { FirstName = "Karabo", LastName = "Kaphwiyo", Email = "u24651797@tuks.co.za", StudentNumber = "24651797" });
+            people.Add(new Models.MyStudy { FirstName = "Dumo", LastName = "Ngobese", Email = "u24742661@tuks.co.za", StudentNumber = "24742661" });
+            people.Add(new Models.MyStudy { FirstName = "Langa", LastName = "Manzini", Email = "u24752038@tuks.co.za", StudentNumber = "24752038" });
+            people.Add(new Models.MyStudy { FirstName = "Gontse", LastName = "Shabangu", Email = "u24698505@tuks.co.za", StudentNumber = "24698505" });
+
+            return View(people);
+        }
+    }
+}
---
+++ Thato1StudyGroup/Models/MyStudy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Thato1StudyGroup.Models
+{
+    public class MyStudy
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your student number")]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+    }
+}
